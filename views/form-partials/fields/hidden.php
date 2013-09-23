0f3a6572b34aa0ab460b8ae9cb3bc2f427f0d7ee
Andrei Lupu <andreilupu>
2013-09-23 12:41:31 +0300

    impl hidden field

---
+++ views/form-partials/fields/hidden.php
@@ -0,0 +1,22 @@
+<?php defined('ABSPATH') or die;
+	/* @var PixcoreFormField $field */
+	/* @var PixcoreForm $form */
+	/* @var mixed $default */
+	/* @var string $name */
+	/* @var string $idname */
+	/* @var string $label */
+	/* @var string $desc */
+	/* @var string $rendering */
+
+	isset($type) or $type = 'text';
+
+	$attrs = array
+		(
+			'name' => $name,
+			'id' => $idname,
+			'type' => 'text',
+			'value' => $form->autovalue($name)
+		);
+?>
+
+<input <?php echo $field->htmlattributes($attrs) ?>/>
